54e8c12adce1ecb730b1d1e3bce75cb3eb81a4a5
Walter Mattingly <waltdakind@gmail.com>
2016-05-15 11:06:21 -0400

    switching to working branch

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,31 @@
 var connection = require('./connection.js');
 
 var orm = {
-    selectByBurger: function () {
-        var burgersToEat = 'SELECT burger_name FROM burgers ORDER BY date desc;';
+    allBurgers: function (table,callback) {
+        var s = 'SELECT * FROM '+ table + 'ORDER BY date desc;';
         connection.query(s, function (err, result) {
 
             console.log(result);
         });
     },
-    findByName: function (burger_name) {
-        var queryString = 'SELECT * FROM burgers WHERE burger_name = ?';
-        connection.query(queryString, [burger_name], function (err, result) {
+    newBurger: function (name, callback) {
+        var q = 'INSERT INTO burgers (burgerName, devoured) VALUES (?, FALSE)';
+        connection.query(newBurger, [burger_name], function (err, result) {
 
             console.log(result);
         });
     },
-    devouredBurgers: function () {
-        var eaten = 'SELECT * FROM burgers WHERE devoured = TRUE';
-
+    devouredBurgers: function (table, callback) {
+        var s = 'SELECT * FROM '+ table + 'WHERE devoured = TRUE;';
         connection.query(s, function (err, result) {
+
             console.log(result);
+        });
+    },
+    devourBurger: function (id,callback) {
+        var q = 'UPDATE burgers SET devoured = FALSE WHERE id = ?';
+        connection.query(q,[id],function(err,results){
+            callback(results);
         });
     }
 };
